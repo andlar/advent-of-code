a727274d7bf566b177bab2a46c91065488eca2ac
Andrew Larned <andrew.larned@gmail.com>
2019-12-24 07:48:33 -0500

    fix: read the actual instructions; sigh

--- src/day15.js
+++ src/day15.js
@@ -3,104 +3,84 @@ import { drawGrid } from './util';
 import { Point } from './point';
 import { program } from './data/day15.data';
 
-const initWorld = () => {
-    let world = {
-        grid: {'0:0': 'o'},
-        droids: [{
-            location: '0:0',
-            history: [],
-            droid: {
-                memory: [...program],
-                input: [],
-                output: [],
-            },
-        }],
-    }
-    return world;
-}
-
-const spawnDroids = droid => {
-    let droids = [];
-    for (let direction = 1; direction <= 4; direction += 1) {
-        let newDroid = {
-            droid: {
-                memory: [...droid.droid.memory],
-                input: [...droid.history, direction],
-                output: [],
-            },
-            history: [...droid.history, direction],
-            location: droid.location,
-        };
-        droids.push(newDroid);
+const initGrid = () => {
+    let grid = {
+        '0:0': 'o',
     };
-    return droids;
+    return grid;
 }
 
-const takeStep = (world, droid) => {
-    let nextWorld = {...world};
-    nextWorld.droids = [];
-    let direction = droid.droid.input[droid.droid.input.length - 1];
-    let nextLocation;
-    switch (direction) {
-    case 1:
-        nextLocation = Point.fromKey(droid.location);
-        nextLocation.y += 1;
-        break;
-    case 2:
-        nextLocation = Point.fromKey(droid.location);
-        nextLocation.x += 1;
-        break;
-    case 3:
-        nextLocation = Point.fromKey(droid.location);
-        nextLocation.y -= 1;
-        break;
-    case 4:
-        nextLocation = Point.fromKey(droid.location);
-        nextLocation.x -= 1;
-        break;
-    default:
-        console.log(world, droid, direction);
-    }
-    //nextWorld.droids = nextWorld.droids.filter(d => d.location !== droid.location);
-    if (!world.grid[nextLocation.key()]) {
-        let nextDroid = {
-            history: [...droid.history],
-            location: nextLocation.key(),
-            droid: runProgram(droid.droid),
-        };
-        switch (nextDroid.droid.output[droid.droid.output.length - 1]) {
-        case 0:
-            nextWorld.grid[nextLocation.key()] = '#';
-            //nextWorld.droids = nextWorld.droids.filter(d => d.location !== droid.location);
+const walk = directions => {
+    let droid = {
+        memory: [...program],
+        input: [...directions],
+        output: [],
+    };
+    let nextDroid = runProgram(droid);
+    let steps = nextDroid.output.map(s => {
+        switch (s) {
+        case 0: return '#';
+        case 1: return '.';
+        default: return '?';
+        }
+    });
+    return steps;
+}
+
+const parseMovement = (directions, steps) => {
+    let grid = {'0:0': 'o'};
+    let location = new Point(0,0);
+    let direction, step;
+    while (directions && directions.length > 0) {
+        let direction = directions.shift();
+        let step = steps.shift();
+        switch (direction) {
+        case 1: //north
+            location.y += 1;
             break;
-        case 1:
-            nextWorld.grid[nextLocation.key()] = '.';
-            nextWorld.droids.push(nextDroid);
+        case 2: //south
+            location.y -= 1;
+            break;
+        case 3: //west
+            location.x -= 1;
+            break;
+        case 4: //east
+            location.x += 1;
             break;
         default:
-            console.log(nextDroid);
+            console.log(grid, direction, step, location);
         }
+        grid[location.key()] = step;
     }
-    return nextWorld;
+    return { grid: grid, lastLocation: location.key() };
 }
 
-const explore = world => {
-    let nextWorld = {...world};
-    let activeDroids = [];
-    let nextDroids = [];
-    world.droids.forEach(d => {
-        activeDroids = activeDroids.concat(spawnDroids(d));
-    });
-    //activeDroids.forEach(d => console.log(d.history, d.droid.input));
-    activeDroids.forEach(d => {
-        nextWorld = takeStep(nextWorld, d);
-        nextDroids = nextDroids.concat(nextWorld.droids);
-    });
-    //nextDroids.forEach(d => console.log(d.history, d.droid.output));
-    nextWorld.droids = nextDroids;
-    //console.log(nextWorld);
-    //console.log(nextWorld.droids.map(d => d.location));
-    return nextWorld;
+const explore = stepCount => {
+    let grid = {};
+    let directions = [];
+    let activePath, steps, output;
+    let pathsToExplore = [[]];
+    let step = 0;
+    while (pathsToExplore && pathsToExplore.length > 0) {
+        directions = pathsToExplore.shift();
+        for (let i = 1; i <= 4; i++) {
+            activePath = [...directions, i];
+            steps = walk(activePath);
+            let endedOnOpenSpace = steps[steps.length - 1] === '.';
+            if (steps[steps.length - 1] === '?') {
+                console.log('found it', steps.length);
+            }
+            output = parseMovement(activePath, steps);
+            if (step < stepCount) {
+                if (endedOnOpenSpace && !grid[output.lastLocation]) {
+                    pathsToExplore.push([...directions, i]);
+                }
+            }
+            grid = {...grid, ...output.grid};
+        }
+        step += 1;
+    }
+    return grid;
 }
 
-export { initWorld, spawnDroids, takeStep, explore };
+export { initGrid, walk, parseMovement, explore };
--- test/day15.test.js
+++ test/day15.test.js
@@ -1,142 +1,109 @@
 import { runProgram } from '../src/intcode';
-import { initWorld, spawnDroids, takeStep, explore } from '../src/day15';
+import { initGrid, walk, parseMovement, explore } from '../src/day15';
 import { drawGrid } from '../src/util';
 
 describe('day 15', () => {
     describe('part 1', () => {
-        it('should initalize a world', () => {
-            let world = initWorld();
-            expect(world.grid).toEqual({'0:0':'o'});
-            expect(world.droids[0].location).toBe('0:0');
-            expect(world.droids[0].history).toEqual([]);
-            expect(world.droids[0].droid.memory.length).toBe(1045);
-            expect(world.droids[0].droid.input).toEqual([]);
-            expect(world.droids[0].droid.output).toEqual([]);
+        it('should initalize a grid', () => {
+            let grid = initGrid();
+            expect(grid).toEqual({'0:0':'o'});
         });
 
-        describe('wrt droids', () => {
-            let world;
-            beforeEach(() => world = initWorld());
-
-            describe('when spawning', () => {
-                it('should spawn droids', () => {
-                    let droids = spawnDroids(world.droids[0]);
-                    expect(droids.length).toBe(4);
-                    for (let i = 0; i < 4; i++) {
-                        expect(droids[i].droid.memory.length).toBe(1045);
-                        expect(droids[i].droid.input.length).toBe(1);
-                    };
-                });
-
-                it('should spawn droids with history', () => {
-                    world.droids[0].history = [1,2,3,4];
-                    let droids = spawnDroids(world.droids[0]);
-                    expect(droids.length).toBe(4);
-                    for (let i = 0; i < 4; i++) {
-                        expect(droids[i].droid.memory.length).toBe(1045);
-                        expect(droids[i].droid.input.length).toBe(5);
-                        expect(droids[i].history.length).toBe(5);
-                    };
-                });
-            });
-
+        describe('droids', () => {
             describe('when moving', () => {
                 it('should go north and hit a wall', () => {
-                    world.droids[0].droid.input.push(1);
-                    world = takeStep(world, world.droids[0]);
-                    expect(world.grid).toEqual({
-                        '0:0': 'o',
-                        '0:1': '#',
-                    })
-                    expect(world.droids.length).toBe(0);
-                });
-
-                it('should go east and hit a wall', () => {
-                    world.droids[0].droid.input.push(2);
-                    world = takeStep(world, world.droids[0]);
-                    expect(world.grid).toEqual({
-                        '0:0': 'o',
-                        '1:0': '#',
-                    })
-                    expect(world.droids.length).toBe(0);
+                    let steps = walk([1]);
+                    expect(steps).toEqual(['#']);
                 });
 
                 it('should go south and hit a wall', () => {
-                    world.droids[0].droid.input.push(3);
-                    world = takeStep(world, world.droids[0]);
-                    expect(world.grid).toEqual({
-                        '0:0': 'o',
-                        '0:-1': '#',
-                    })
-                    expect(world.droids.length).toBe(0);
+                    let steps = walk([2]);
+                    expect(steps).toEqual(['#']);
                 });
 
-                it('should go west and find a space', () => {
-                    world.droids[0].droid.input.push(4);
-                    world = takeStep(world, world.droids[0]);
-                    expect(world.grid).toEqual({
-                        '0:0': 'o',
-                        '-1:0': '.',
-                    })
-                    expect(world.droids.length).toBe(1);
-                    expect(world.droids[0].location).toBe('-1:0');
+                it('should go west and hit a wall', () => {
+                    let steps = walk([3]);
+                    expect(steps).toEqual(['#']);
                 });
 
-                it('should stop if it finds an explored space', () => {
-                    world.droids[0].droid.input.push(4);
-                    world.grid['-1:0'] = '.';
-                    world = takeStep(world, world.droids[0]);
-                    expect(world.droids.length).toBe(0);
+                it('should go east and find a space', () => {
+                    let steps = walk([4]);
+                    expect(steps).toEqual(['.']);
                 });
 
-                it('should go a few steps', () => {
-                    world.droids[0].droid.input = [4, 3, 4, 3, 4]
-                    world = takeStep(world, world.droids[0]);
-                    world = takeStep(world, world.droids[0]);
-                    world = takeStep(world, world.droids[0]);
-                    world = takeStep(world, world.droids[0]);
-                    console.log(drawGrid(world.grid));
-                    expect(world.grid).toEqual({
-                        '0:0': 'o',
-                        '-1:0': '.',
-                    })
-                    expect(world.droids.length).toBe(1);
-                    expect(world.droids[0].location).toBe('-1:0');
+                it('should take two steps', () => {
+                    let steps = walk([4, 4]);
+                    expect(steps).toEqual(['.', '.']);
                 });
 
-            });
-
-            describe('when exploring', () => {
-                it('should take steps', () => {
-                    let nextWorld = explore(world);
-                    expect(world.grid).toEqual({
-                        '0:0': 'o',
-                        '-1:0': '.',
-                        '1:0': '#',
-                        '0:1': '#',
-                        '0:-1': '#',
-                    });
-                    nextWorld = explore(nextWorld);
-                    expect(world.grid).toEqual({
-                        '0:0': 'o',
-                        '-1:0': '.',
-                        '1:0': '#',
-                        '0:1': '#',
-                        '0:-1': '#',
-                        '-1:-1': '.',
-                        '-1:1': '#',
-                        '-2:0': '#',
-                    });
+                it('should take three steps', () => {
+                    let steps = walk([4, 4, 1]);
+                    expect(steps).toEqual(['.', '.', '#']);
                 });
             });
 
-            xit('should find the oxygen', () => {
-                let nextWorld = explore(world);
-                for (let i = 0; i < 5; i++) {
-                    nextWorld = explore(nextWorld);
-                }
-                console.log(drawGrid(nextWorld.grid), nextWorld.droids.length);
-                //nextWorld.droids.forEach(d => console.log(d.history, d.droid.output, d.droid.input));
+            describe('when interpreting movement', () => {
+                let directions, steps;
+
+                it('should build a map of results with simple steps', () => {
+                    directions = [1];
+                    steps = ['#'];
+                    let output = parseMovement(directions, steps);
+                    expect(output.grid).toEqual({
+                        '0:0': 'o',
+                        '0:1': '#',
+                    });
+                    expect(output.lastLocation).toBe('0:1');
+                });
+
+                it('should build a map of results with a few steps', () => {
+                    directions = [4, 4, 1];
+                    steps = ['.', '.', '#'];
+                    let output = parseMovement(directions, steps);
+                    expect(output.grid).toEqual({
+                        '0:0': 'o',
+                        '1:0': '.',
+                        '2:0': '.',
+                        '2:1': '#',
+                    });
+                    expect(output.lastLocation).toBe('2:1');
+                });
+            });
+        });
+
+        describe('when exploring', () => {
+            it('should explore one step', () => {
+                let grid = explore(0);
+                expect(grid).toEqual({
+                    '0:0': 'o',
+                    '-1:0': '#',
+                    '1:0': '.',
+                    '0:1': '#',
+                    '0:-1': '#',
+                });
+            });
+
+            it('should explore three steps', () => {
+                let grid = explore(2);
+                expect(grid).toEqual({
+                    '-1:0': '#',
+                    '0:-1': '#',
+                    '0:0': 'o',
+                    '0:1': '#',
+                    '1:0': '.',
+                    '1:1': '#',
+                    '1:-1': '#',
+                    '2:-1': '.',
+                    '2:0': '.',
+                    '2:1': '#',
+                    '3:0': '#',
+                });
+            });
+
+            it('should explore more steps', () => {
+                let grid = explore(420);
+                console.log(drawGrid(grid));
+                expect(Object.keys(grid).length).toBe(932);
             });
         });
     });
