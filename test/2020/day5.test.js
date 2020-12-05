d0af772fd704de978d4efbdce5928cc85a525a2b
Andrew Larned <andrew.larned@gmail.com>
2020-12-05 11:11:09 -0500

    test: arrange tests by functionality

--- test/2020/day5.test.js
+++ test/2020/day5.test.js
@@ -1,25 +1,27 @@
 import { getSeatId, ticketCheck, findSeat } from '../../src/2020/day5';
 import { tickets } from '../../src/2020/data/day5';
 
-describe('day 5a tests', () => {
-    it('should get seat ids', () => {
-        expect(getSeatId('FBFBBFFRLR')).toBe(357);
-        expect(getSeatId('BFFFBBFRRR')).toBe(567);
-        expect(getSeatId('FFFBBBFRRR')).toBe(119);
-        expect(getSeatId('BBFFBBFRLL')).toBe(820);
+describe('the day 5', () => {
+    describe('utility functions', () => {
+        it('should get seat ids', () => {
+            expect(getSeatId('FBFBBFFRLR')).toBe(357);
+            expect(getSeatId('BFFFBBFRRR')).toBe(567);
+            expect(getSeatId('FFFBBBFRRR')).toBe(119);
+            expect(getSeatId('BBFFBBFRLL')).toBe(820);
+        });
+
+        it('should find the highest id', () => {
+            expect(ticketCheck(['FBFBBFFRLR', 'BFFFBBFRRR','FFFBBBFRRR', 'BBFFBBFRLL'])).toBe(820);
+        });
     });
 
-    it('should find the highest id', () => {
-        expect(ticketCheck(['FBFBBFFRLR', 'BFFFBBFRRR','FFFBBBFRRR', 'BBFFBBFRLL'])).toBe(820);
-    });
+    describe('solutions', () => {
+        it('should find the highest id of real data', () => {
+            expect(ticketCheck(tickets)).toBe(926);
+        });
 
-    it('should find the highest id of real data', () => {
-        expect(ticketCheck(tickets)).toBe(926);
-    });
-});
-
-describe('day 4b tests', () => {
-    it('should find my seat', () => {
-        expect(findSeat(tickets)).toBe(657);
+        it('should find my seat', () => {
+            expect(findSeat(tickets)).toBe(657);
+        });
     });
 });
