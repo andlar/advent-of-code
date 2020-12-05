3285f3e6230eb26263f8743ab6802919c2d77a9b
Andrew Larned <andrew.larned@gmail.com>
2020-12-05 11:08:40 -0500

    refactor: recognize the boarding passes are literally just binary

--- src/2020/day5.js
+++ src/2020/day5.js
@@ -1,26 +1,10 @@
-const startSeat = {rowMin: 0, rowMax: 127, colMin: 0, colMax: 7};
+const getSeatId = ticket => parseInt(ticket.replace(/[FL]/g, 0).replace(/[BR]/g, 1), 2);
 
-const getSeat = (ticket, seat = startSeat) => {
-    switch(ticket.charAt(0)) {
-    case 'F': return getSeat(ticket.substring(1), {...seat, rowMax: seat.rowMax - (seat.rowMax - seat.rowMin + 1) / 2,});
-    case 'B': return getSeat(ticket.substring(1), {...seat, rowMin: seat.rowMin + (seat.rowMax - seat.rowMin + 1) / 2,});
-    case 'R': return getSeat(ticket.substring(1), {...seat, colMin: seat.colMin + (seat.colMax - seat.colMin + 1) / 2,});
-    case 'L': return getSeat(ticket.substring(1), {...seat, colMax: seat.colMax - (seat.colMax - seat.colMin + 1) / 2,});
-    default: return {row: seat.rowMin, col: seat.colMin, id: seat.rowMin * 8 + seat.colMin};
-    }
-}
+const ticketCheck = tickets => Math.max(...tickets.map(t => getSeatId(t)));
 
-const ticketCheck = tickets => Math.max(...tickets.map(t => getSeat(t).id));
+const findSeat = tickets => tickets
+      .map(t => getSeatId(t))
+      .sort((a, b) => a < b ? -1 : 1)
+      .find((s, idx, arr) => idx !== 0 && s !== arr[idx - 1] + 1) - 1;
 
-const findSeat = tickets => {
-    let seats = tickets
-        .map(t => getSeat(t))
-        .sort((a, b) => a.id < b.id ? -1 : 1)
-        .filter((s, idx, arr) => idx === 0
-                || idx === arr.length - 1
-                || s.id !== arr[idx - 1].id + 1
-                || s.id !== arr[idx + 1].id - 1);
-    return seats[1].id + 1;
-}
-
-export { getSeat, ticketCheck, findSeat };
+export { getSeatId, ticketCheck, findSeat };
--- test/2020/day5.test.js
+++ test/2020/day5.test.js
@@ -1,12 +1,12 @@
-import { getSeat, ticketCheck, findSeat } from '../../src/2020/day5';
+import { getSeatId, ticketCheck, findSeat } from '../../src/2020/day5';
 import { tickets } from '../../src/2020/data/day5';
 
 describe('day 5a tests', () => {
-    it('parse tickets to get a seat', () => {
-        expect(getSeat('FBFBBFFRLR')).toEqual({row: 44, col: 5, id: 357});
-        expect(getSeat('BFFFBBFRRR')).toEqual({row: 70, col: 7, id: 567});
-        expect(getSeat('FFFBBBFRRR')).toEqual({row: 14, col: 7, id: 119});
-        expect(getSeat('BBFFBBFRLL')).toEqual({row: 102, col: 4, id: 820});
+    it('should get seat ids', () => {
+        expect(getSeatId('FBFBBFFRLR')).toBe(357);
+        expect(getSeatId('BFFFBBFRRR')).toBe(567);
+        expect(getSeatId('FFFBBBFRRR')).toBe(119);
+        expect(getSeatId('BBFFBBFRLL')).toBe(820);
     });
 
     it('should find the highest id', () => {
