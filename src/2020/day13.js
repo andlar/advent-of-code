102d16c72f28acdf2728a0e8518921f16799d61c
Andrew Larned <andrew.larned@gmail.com>
2020-12-14 07:42:50 -0500

    wip: try to find when the busses return for day 13 part 2

--- src/2020/day13.js
+++ src/2020/day13.js
@@ -9,4 +9,32 @@ const nextBus = input => {
     return busses[0];
 }
 
-export { nextBus };
+const sameTime = (busses, t) => busses
+      .split(',')
+      .reduce((same, b, idx) => same && (b === 'x' || ((t + idx) % b === 0)), true);
+
+const findTime = busses => {
+    let t = 0;
+    let inc = 0;//parseInt(busses.split(',').filter(b => b !== 'x')[0], 10);
+    busses.split(',').forEach((bus, idx) => {
+        if (bus === 'x') { return ;}
+        let b = parseInt(bus, 10);
+        if (b > inc) {
+            inc = b;
+            t = idx * -1;
+        }
+    });
+    //console.log({t, inc, busses});
+    while (!sameTime(busses, t)) {
+        /*
+        if (t > 3400) {
+            console.log({t, inc, busses, out: sameTime(busses, t)});
+        }
+        */
+        t += inc;
+        //if (t > 3420) { break; }
+    }
+    return t;
+}
+
+export { nextBus, sameTime, findTime };
--- test/2020/day13.test.js
+++ test/2020/day13.test.js
@@ -1,4 +1,4 @@
-import { nextBus } from '../../src/2020/day13';
+import { nextBus, sameTime, findTime } from '../../src/2020/day13';
 import { input } from '../../src/2020/data/day13';
 
 let mock = `939
@@ -9,11 +9,53 @@ describe('utility functions', () => {
         let bus = nextBus(mock);
         expect(bus.bus * bus.time).toEqual(295);
     });
+
+    it('should know when busses are departing at the right time', () => {
+        expect(sameTime(mock[1], 0)).toBe(false);
+        expect(sameTime(mock[1], 1068781)).toBe(true);
+    });
+
+    it('should know other times', () => {
+        expect(sameTime('17,x,13,19', 3417)).toBe(true);
+        expect(sameTime('67,7,59,61', 754018)).toBe(true);
+        expect(sameTime('67,x,7,59,61', 779210)).toBe(true);
+        expect(sameTime('67,7,x,59,61', 1261476)).toBe(true);
+        expect(sameTime('1789,37,47,1889', 1202161486)).toBe(true);
+    });
+
+    it('should find the time', () => {
+        expect(findTime(mock[1])).toBe(1068781);
+    });
+
+    it('should find other times', () => {
+        expect(findTime('17,x,13,19')).toBe(3417);
+    });
+
+    it('should find other times', () => {
+        expect(findTime('67,7,59,61')).toBe(754018);
+    });
+
+    it('should find other times', () => {
+        expect(findTime('67,x,7,59,61')).toBe(779210);
+    });
+
+    it('should find other times', () => {
+        expect(findTime('67,7,x,59,61')).toBe(1261476);
+    });
+
+    it('should find other times', () => {
+        expect(findTime('1789,37,47,1889')).toBe(1202161486);
+    });
 });
 
 describe('solutions', () => {
     it('should solve day 1', () => {
         let bus = nextBus(input);
         expect(bus.bus * bus.time).toEqual(3865);
+    });
+
+    xit('should solve day 2', () => {
+        let time = findTime(input[1]);
+        expect(time).toEqual(3865);
     });
 });
