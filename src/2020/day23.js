2f05152b937928c12b6c3fd6b8da4454f32de41d
Andrew Larned <andrew.larned@gmail.com>
2020-12-23 16:47:27 -0500

    refactor: convert to set instead of array for cup game, but somehow get the wrong answer

--- src/2020/day23.js
+++ src/2020/day23.js
@@ -1,38 +1,48 @@
-const step = (cups) => {
-    let removed = cups.splice(1, 3);
-    let destination = cups[0] - 1;
-    while (removed.indexOf(destination) > -1 || destination === 0) {
-        destination -= 1;
-        if (destination <= 0) {
-            destination = cups.length + 3;
+const makeMap = input => {
+    let cups = new Map();
+    input.split('').map(c => parseInt(c, 10)).forEach((cup, idx, array) => {
+        if (idx === array.length - 1) {
+            cups.set(cup, array[0]);
+        } else {
+            cups.set(cup, array[idx + 1]);
         }
-    }
-    cups.splice(cups.indexOf(destination) + 1, 0, removed[0], removed[1], removed[2]);
-    cups.push(cups[0]);
-    cups.shift();
+    });
     return cups;
 }
 
-const play = (cups, steps) => {
+const step = (cups, current, max) => {
+    let removed = cups.get(current);
+    let skipped = cups.get(removed);
+    let last = cups.get(skipped);
+    cups.set(current, cups.get(last));
+    let destination = current - 1;
+    while (destination === removed || destination === skipped || destination === last || destination === 0) {
+        destination -= 1;
+        if (destination <= 0) {
+            destination = max;
+        }
+    }
+    cups.set(last, cups.get(destination));
+    cups.set(destination, removed);
+    return cups;
+}
+
+const play = (cups, steps, current, max) => {
     for (let i = 0; i < steps; i++) {
-        cups = step(cups);
+        cups = step(cups, current, max);
+        current = cups.get(current);
     }
     return cups;
 }
 
 const getAnswer = cups => {
-    while (cups[0] !== 1) {
-        cups.push(cups[0]);
-        cups.shift();
-    }
-    return cups.slice(1).join('');
-}
-
-const buildCircle = (cups, total) => {
-    for (let i = cups.length + 1; i <= total; i++) {
-        cups.push(i);
-    }
-    return cups;
+    let next = 1;
+    let out = '';
+    do {
+        out += cups.get(next);
+        next = cups.get(next);
+    } while (next !== 1)
+    return out.substring(0, out.length - 1);
 }
 
 const getSecondAnswer = cups => {
@@ -43,4 +53,4 @@ const getSecondAnswer = cups => {
     return cups[1] * cups[2];
 }
 
-export { step, play, getAnswer, buildCircle, getSecondAnswer };
+export { makeMap, step, play, getAnswer, getSecondAnswer };
--- test/2020/day23.test.js
+++ test/2020/day23.test.js
@@ -1,53 +1,116 @@
-import { step, play, getAnswer, buildCircle, getSecondAnswer } from '../../src/2020/day23';
+import { makeMap, step, play, getAnswer, getSecondAnswer } from '../../src/2020/day23';
 
 let mock = '389125467';
 let real = '459672813';
-let cups;
 
 describe('utility functions', () => {
-    beforeEach(() => {
-        cups = mock.split('').map(c => parseInt(c, 10));
+    let cups;
+    it('should make a map', () => {
+        let cups = makeMap(mock);
+        expect(cups.get(3)).toBe(8);
+        expect(cups.get(8)).toBe(9);
+        expect(cups.get(9)).toBe(1);
+        expect(cups.get(1)).toBe(2);
+        expect(cups.get(2)).toBe(5);
+        expect(cups.get(5)).toBe(4);
+        expect(cups.get(4)).toBe(6);
+        expect(cups.get(6)).toBe(7);
+        expect(cups.get(7)).toBe(3);
     });
 
-    it('should take a step', () => {
-        let result = step(cups);
-        expect(result).toEqual([2,8,9,1,5,4,6,7,3]);
+    describe('given a map', () => {
+        beforeEach(() => {
+            cups = makeMap(mock);
+        });
+
+        it('should take a step', () => {
+            let result = step(cups, 3, 9);
+            expect(cups.get(1)).toBe(5);
+            expect(cups.get(2)).toBe(8);
+            expect(cups.get(3)).toBe(2);
+            expect(cups.get(4)).toBe(6);
+            expect(cups.get(5)).toBe(4);
+            expect(cups.get(6)).toBe(7);
+            expect(cups.get(7)).toBe(3);
+            expect(cups.get(8)).toBe(9);
+            expect(cups.get(9)).toBe(1);
+        });
+
+        it('should take a wrapping step', () => {
+            let tst = makeMap('136792584');
+            let result = step(tst, 1, 9);
+            expect(result.get(1)).toBe(9);
+            expect(result.get(2)).toBe(5);
+            expect(result.get(3)).toBe(6);
+            expect(result.get(4)).toBe(1);
+            expect(result.get(5)).toBe(8);
+            expect(result.get(6)).toBe(7);
+            expect(result.get(7)).toBe(2);
+            expect(result.get(8)).toBe(4);
+            expect(result.get(9)).toBe(3);
+        });
+
+        it('should skip removed items', () => {
+            let tst = makeMap('328915467');
+            let result = step(tst, 2, 9);
+            expect(result.get(1)).toBe(3);
+            expect(result.get(2)).toBe(5);
+            expect(result.get(3)).toBe(2);
+            expect(result.get(4)).toBe(6);
+            expect(result.get(5)).toBe(4);
+            expect(result.get(6)).toBe(7);
+            expect(result.get(7)).toBe(8);
+            expect(result.get(8)).toBe(9);
+            expect(result.get(9)).toBe(1);
+        });
+
+        it('should play a game of ten steps', () => {
+            let result = play(cups, 10, 3, 9);
+            expect(result.get(1)).toBe(9);
+            expect(result.get(2)).toBe(6);
+            expect(result.get(3)).toBe(7);
+            expect(result.get(4)).toBe(1);
+            expect(result.get(5)).toBe(8);
+            expect(result.get(6)).toBe(5);
+            expect(result.get(7)).toBe(4);
+            expect(result.get(8)).toBe(3);
+            expect(result.get(9)).toBe(2);
+        });
+
+        it('should get an answer', () => {
+            let result = play(cups, 10, 3, 9);
+            let answer = getAnswer(result);
+            expect(answer).toBe('92658374');
+        });
+
+        it('should get an answer after 100 turns', () => {
+            let result = play(cups, 100, 3, 9);
+            let answer = getAnswer(result);
+            expect(answer).toBe('67384529');
+        });
     });
 
-    it('should take a wrapping step', () => {
-        let tst = [1,3,6,7,9,2,5,8,4];
-        let result = step(tst);
-        expect(result).toEqual([9,3,6,7,2,5,8,4,1]);
+    xit('should build a bigger circle', () => {
+        cups = makeMap(mock, 20);
+        expect(cups.get(3)).toBe(8);
+        expect(cups.get(8)).toBe(9);
+        expect(cups.get(7)).toBe(10);
+        expect(cups.get(20)).toBe(3);
     });
 
-    it('should play a game of ten steps', () => {
-        let result = play(cups, 10);
-        expect(result).toEqual([8,3,7,4,1,9,2,6,5]);
-    });
-
-    it('should get an answer', () => {
-        let answer = getAnswer([8,3,7,4,1,9,2,6,5]);
-        expect(answer).toBe('92658374');
-    });
-
-    it('should build a bigger circle', () => {
-        cups = buildCircle(cups, 20);
-        expect(cups).toEqual([3,8,9,1,2,5,4,6,7,10,11,12,13,14,15,16,17,18,19,20])
-    });
-
-    it('should build a much bigger circle', () => {
-        cups = buildCircle(cups, 1000000);
+    xit('should build a much bigger circle', () => {
+        cups = makeMap(mock, 1000000);
         expect(cups.length).toBe(1000000);
         expect(cups[cups.length - 1]).toBe(1000000);
     });
 
-    it('should get the second answer', () => {
+    xit('should get the second answer', () => {
         let answer = getSecondAnswer([8,3,7,4,1,9,2,6,5]);
         expect(answer).toBe(18);
     });
 
     xit('should play a much bigger game', () => {
-        cups = buildCircle(cups, 1000000);
+        cups = makeMap(mock, 1000000);
         let result = play(cups, 10000000);
         let answer = getSecondAnswer(result);
         expect(answer).toBe(149245887792);
@@ -55,17 +118,16 @@ describe('utility functions', () => {
 });
 
 describe('solutions', () => {
-    beforeEach(() => {
-        cups = real.split('').map(c => parseInt(c, 10));
-    });
-
     it('should play game for 100 steps', () => {
-        let answer = getAnswer(play(cups, 100));
+        let cups = makeMap(real);
+        let result = play(cups, 100, 3, 9);
+        let answer = getAnswer(result);
+        console.log({result, answer});
         expect(answer).toBe('68245739');
     });
 
     xit('should play the bigger game', () => {
-        cups = buildCircle(cups, 1000000);
+        let cups = makeMap(real, 1000000);
         let answer = getSecondAnswer(play(cups, 10000000, 1000000));
         expect(answer).toBe(149245887792);
     });
