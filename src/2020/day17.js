d21f4e3b6e2fc7ac6f5dde50c82031514375b82e
Andrew Larned <andrew.larned@gmail.com>
2020-12-27 16:22:15 -0500

    feat: count hyperspace cubes in 4 dimensions for part 2 of day 17

--- src/2020/day17.js
+++ src/2020/day17.js
@@ -10,6 +10,18 @@ const initSpace = input => {
     return space;
 }
 
+const init4dSpace = input => {
+    let space = new Set();
+    input.forEach((row, y) => {
+        row.split('').forEach((col, x) => {
+            if (col === '#') {
+                space.add(x + ':' + y + ':0:0');
+            }
+        });
+    });
+    return space;
+}
+
 const getNeighbors = (space, loc) => {
     let [x, y, z] = loc.split(':').map(v => parseInt(v, 10));
     let count = 0;
@@ -20,6 +32,24 @@ const getNeighbors = (space, loc) => {
                 if (space.has((x + dx) + ':' + (y + dy) + ':' + (z + dz))) {
                     count++;
                 }
+            });
+        });
+    });
+    return count;
+}
+
+const get4dNeighbors = (space, loc) => {
+    let [x, y, z, w] = loc.split(':').map(v => parseInt(v, 10));
+    let count = 0;
+    [-1, 0, 1].forEach(dx => {
+        [-1, 0, 1].forEach(dy => {
+            [-1, 0, 1].forEach(dz => {
+                [-1, 0, 1].forEach(dw => {
+                    if (dx === 0 && dy === 0 && dz === 0 && dw === 0) { return; }
+                    if (space.has((x + dx) + ':' + (y + dy) + ':' + (z + dz) + ':' + (w + dw))) {
+                        count++;
+                    }
+                });
             });
         });
     });
@@ -57,6 +87,39 @@ const iterate = space => {
     return next;
 }
 
+const iterate4d = space => {
+    let next = new Set();
+    let neighbors = new Set();
+    space.forEach(value => {
+        let [x, y, z, w] = value.split(':').map(v => parseInt(v, 10));
+        [-1, 0, 1].forEach(dx => {
+            [-1, 0, 1].forEach(dy => {
+                [-1, 0, 1].forEach(dz => {
+                    [-1, 0, 1].forEach(dw => {
+                        let key = (x + dx) + ':' + (y + dy) + ':' + (z + dz) + ':' + (w + dw);
+                        if (dx === 0 && dy === 0 && dz === 0 && dw === 0) {
+                            let cnt = get4dNeighbors(space, key);
+                            if (2 === cnt || cnt === 3) {
+                                next.add(key);
+                            }
+                        }
+                        if (!space.has(key)) {
+                            neighbors.add(key)
+                        }
+                    });
+                });
+            });
+        });
+    });
+    neighbors.forEach(check => {
+        let cnt = get4dNeighbors(space, check);
+        if (cnt === 3) {
+            next.add(check);
+        }
+    });
+    return next;
+}
+
 const grow = (space, steps) => {
     for (let i = 0; i < steps; i++) {
         space = iterate(space);
@@ -64,11 +127,11 @@ const grow = (space, steps) => {
     return space;
 }
 
-const growIn4d = (grid, steps) => {
+const grow4d = (space, steps) => {
     for (let i = 0; i < steps; i++) {
-        grid = iterateIn4d(grid).grid;
+        space = iterate4d(space);
     }
-    return grid;
+    return space;
 }
 
-export { initSpace, getNeighbors, iterate, grow };
+export { initSpace, init4dSpace, getNeighbors, get4dNeighbors, iterate, iterate4d, grow, grow4d };
--- test/2020/day17.test.js
+++ test/2020/day17.test.js
@@ -1,4 +1,4 @@
-import { initSpace, getNeighbors, iterate, grow } from '../../src/2020/day17';
+import { initSpace, init4dSpace, getNeighbors, get4dNeighbors, iterate, iterate4d, grow, grow4d } from '../../src/2020/day17';
 import { input } from '../../src/2020/data/day17';
 
 let mock = `.#.
@@ -6,7 +6,7 @@ let mock = `.#.
 ###`.split('\n');
 
 describe('utility functions', () => {
-    it('should init space', () => {
+    it('should init 3-D space', () => {
         let space = initSpace(mock);
         expect(space.has('0:0:0')).toBe(false);
         expect(space.has('1:0:0')).toBe(true);
@@ -14,7 +14,15 @@ describe('utility functions', () => {
         expect(space.has('2:2:0')).toBe(true);
     });
 
-    describe('given space', () => {
+    it('should init 4-D space', () => {
+        let space = init4dSpace(mock);
+        expect(space.has('0:0:0:0')).toBe(false);
+        expect(space.has('1:0:0:0')).toBe(true);
+        expect(space.has('2:1:0:0')).toBe(true);
+        expect(space.has('2:2:0:0')).toBe(true);
+    });
+
+    describe('given 3-D space', () => {
         let space;
         beforeEach(() => {
             space = initSpace(mock);
@@ -38,6 +46,31 @@ describe('utility functions', () => {
             expect(space.size).toBe(112);
         });
     });
+
+    describe('given 4-D space', () => {
+        let space;
+        beforeEach(() => {
+            space = init4dSpace(mock);
+        });
+
+        it('should get neighbors', () => {
+            expect(get4dNeighbors(space, '0:0:0:0')).toEqual(1); // x, y, z
+            expect(get4dNeighbors(space, '1:0:0:0')).toEqual(1);
+            expect(get4dNeighbors(space, '2:0:0:0')).toEqual(2);
+            expect(get4dNeighbors(space, '1:1:0:0')).toEqual(5);
+            expect(get4dNeighbors(space, '2:2:0:0')).toEqual(2);
+        });
+
+        it('should iterate once', () => {
+            space = iterate4d(space);
+            expect(space.size).toBe(29);
+        });
+
+        it('should grow', () => {
+            space = grow4d(space, 6);
+            expect(space.size).toBe(848);
+        });
+    });
 });
 
 describe('solutions', () => {
@@ -47,8 +80,9 @@ describe('solutions', () => {
         expect(space.size).toBe(333);
     });
 
-    xit('should know how many cubes exist in 4d space after 6 turns', () => {
-        let next = growIn4d([input], 6);
-        expect(countCubesIn4d(next)).toEqual(333); //687 is too low
+    it('should know how many cubes there are after 6 turns', () => {
+        let space = init4dSpace(input);
+        space = grow4d(space, 6);
+        expect(space.size).toBe(2676);
     });
 });
