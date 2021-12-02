bcd759b21c50f414ddd15ce435550a70d1a9e159
Andrew Larned <andrew.larned@gmail.com>
2021-12-02 05:54:20 -0500

    refactor: simplify logic in depth finding

--- src/2021/01.js
+++ src/2021/01.js
@@ -1,10 +1,7 @@
-const parseData = (input) => input.split('\n').map((val) => parseInt(val, 10));
-
-const countIncreases = (input) => input
-      .filter((val, idx, arr) => val > arr[idx - 1])
+const countIncreases = (input, diff = 1) => input
+      .split('\n')
+      .map((val) => parseInt(val, 10))
+      .filter((val, idx, arr) => val > arr[idx - diff])
       .length;
 
-const convertToWindows = (input) => parseData(input)
-      .map((val, idx, arr) => val + arr[idx + 1] + arr[idx + 2]);
-
-export { parseData, countIncreases, convertToWindows };
+export { countIncreases };
--- test/2021/01.test.js
+++ test/2021/01.test.js
@@ -1,33 +1,23 @@
-import { countIncreases, convertToWindows, parseData } from '2021/01';
+import { countIncreases } from '2021/01';
 import { mock, entries } from '2021/01.data';
 
 describe('day 1 tests', () => {
-  it('should parse input as numbers', () => {
-    expect(parseData(mock)).toEqual([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
-  });
-
   it('should count increases in mock data', () => {
-    const parsed = parseData(mock);
-    expect(countIncreases(parsed)).toEqual(7);
+    expect(countIncreases(mock)).toEqual(7);
   });
 
   it('should count increases in real data', () => {
-    const parsed = parseData(entries);
-    expect(countIncreases(parsed)).toBeGreaterThan(1531);
-    expect(countIncreases(parsed)).toBe(1532);
+    expect(countIncreases(entries)).toBeGreaterThan(1531);
+    expect(countIncreases(entries)).toBe(1532);
   });
 });
 
 describe('day 1 part 2 tests', () => {
-  it('should convert data to windows', () => {
-    expect(convertToWindows(mock)).toEqual([607, 618, 618, 617, 647, 716, 769, 792, NaN, NaN]);
+  it('should count offset increases in mock data', () => {
+    expect(countIncreases(mock, 3)).toEqual(5);
   });
 
-  it('should count increase in mock windows', () => {
-    expect(countIncreases(convertToWindows(mock))).toEqual(5);
-  });
-
-  it('should count increase in real windows', () => {
-    expect(countIncreases(convertToWindows(entries))).toEqual(1571);
+  it('should count offset increases in real data', () => {
+    expect(countIncreases(entries, 3)).toEqual(1571);
   });
 });
