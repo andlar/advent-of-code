9556d03be2f0fe81fe53e532aa312fe77e794dd0
Andrew Larned <andrew.larned@gmail.com>
2021-12-09 22:17:28 -0500

    feat: find largest basins of smoke

--- src/2021/09.js
+++ src/2021/09.js
@@ -3,27 +3,84 @@ const parseMap = (input) => ({
   locations: input
     .join('')
     .split('')
-    .map((cell) => parseInt(cell, 10)),
-})
+    .map((cell, idx) => ({
+      value: parseInt(cell, 10),
+      drainsTo: undefined,
+    }))
+});
 
-const findLowpoints = (state) => {
-  let out = state.locations.filter((cell, idx) => {
-    let lowest = true;
-    lowest = lowest && cell < (state.locations[idx - state.mod] ?? 99); // n
-    lowest = lowest && (idx % state.mod === state.mod - 1 ? true : cell < state.locations[idx + 1]); // e
-    lowest = lowest && cell < (state.locations[idx + state.mod] ?? 99); // s
-    lowest = lowest && (idx % state.mod === 0 ? true : cell < state.locations[idx - 1]); // w
-    return lowest;
+const findBasins = (state) => ({
+  ...state,
+  locations: state.locations
+    .map((cell, idx) => ({
+      ...cell,
+      drainsTo: true
+        && cell.value < (state.locations[idx - state.mod]?.value ?? 9) // n
+        && (idx % state.mod === state.mod - 1 ? true : cell.value < state.locations[idx + 1].value) // e
+        && cell.value < (state.locations[idx + state.mod]?.value ?? 9) // s
+        && (idx % state.mod === 0 ? true : cell.value < state.locations[idx - 1].value) // w
+        ? idx : undefined,
+    })),
+});
+
+const findLowpoints = (state) => findBasins(state).locations
+      .filter((cell, idx) => cell.drainsTo === idx)
+      .map((cell) => cell.value);
+
+const expandSmoke = (state) => {
+  const out = {
+    ...state,
+    mapped: true,
+  };
+  out.locations = state.locations.map((cell, idx) => {
+    if (cell.drainsTo || cell.value === 9) { return cell; }
+    out.mapped = false;
+    cell.drainsTo =
+      state.locations[idx - state.mod]?.drainsTo // n
+      || (idx % state.mod !== state.mod - 1 && state.locations[idx + 1].drainsTo) // e
+      || state.locations[idx + state.mod]?.drainsTo // s
+      || (idx % state.mod !== 0 && state.locations[idx - 1].drainsTo) // w
+    ;
+    if (cell.drainsTo) {
+      out.locations[cell.drainsTo].basinSize = (out.locations[cell.drainsTo].basinSize ?? 1) + 1;
+    }
+    return cell;
   });
   return out;
 }
 
-const findRiskLevel = (state) => {
-  return state.reduce((total, val) => total + val + 1, 0);
+const mapBasins = (state) => {
+  let next = findBasins(state);
+  while (!next.mapped) {
+    next = expandSmoke(next);
+  }
+  return next.locations.filter((cell) => cell.basinSize);
 }
+
+const getMap = (state) => {
+  return state.locations.map((cell, idx) => {
+    let char = cell.value;
+    if (cell.value === 9) {
+      char = '#';
+    } else if (cell.drainsTo && cell.drainsTo !== idx) {
+      char = '.';
+    }
+    if (idx % state.mod === state.mod - 1) {
+      char += '\n';
+    }
+    return char;
+  }).join('');
+}
+
+const findRiskLevel = (state) => state.reduce((total, val) => total + val + 1, 0);
+
+const calculateBasins = (state) => state.map((cell) => cell.basinSize).sort((a, b) => b - a).slice(0, 3).reduce((total, val) => total * val, 1);
 
 export {
   parseMap,
   findLowpoints,
   findRiskLevel,
+  mapBasins,
+  getMap,
+  calculateBasins,
 };
--- test/2021/09.test.js
+++ test/2021/09.test.js
@@ -2,16 +2,19 @@ import {
   parseMap,
   findLowpoints,
   findRiskLevel,
+  mapBasins,
+  getMap,
+  calculateBasins,
 } from '2021/09';
 import { mock, real } from '2021/09.data';
 
 describe('while parsing data', () => {
   it('should interpret input', () => {
     let state = parseMap(mock);
-    expect(state.locations[0]).toBe(2);
-    expect(state.locations[9]).toBe(0);
-    expect(state.locations[40]).toBe(9);
-    expect(state.locations[49]).toBe(8);
+    expect(state.locations[0]).toEqual({value: 2, drainsTo: undefined});
+    expect(state.locations[9]).toEqual({value: 0, drainsTo: undefined});
+    expect(state.locations[40]).toEqual({value: 9, drainsTo: undefined});
+    expect(state.locations[49]).toEqual({value: 8, drainsTo: undefined});
     expect(state.mod).toBe(10);
   });
 });
@@ -29,6 +32,21 @@ describe('with mock data', () => {
   it('should find the risk level', () => {
     expect(findRiskLevel([1, 0, 5, 5])).toBe(15);
   });
+
+  it('should map basins', () => {
+    state = mapBasins(state);
+    expect(state).toEqual([
+      { value: 1, drainsTo: 1, basinSize: 3 },
+      { value: 0, drainsTo: 9, basinSize: 9 },
+      { value: 5, drainsTo: 22, basinSize: 14 },
+      { value: 5, drainsTo: 46, basinSize: 9 }
+    ]);
+  });
+
+  it('should calculate basin size', () => {
+    state = calculateBasins(mapBasins(state));
+    expect(state).toBe(1134);
+  });
 });
 
 describe('with real data', () => {
@@ -40,5 +58,10 @@ describe('with real data', () => {
   it('should find the risk level', () => {
     expect(findRiskLevel(findLowpoints(state))).toBeGreaterThan(616);
     expect(findRiskLevel(findLowpoints(state))).toBe(631);
+  });
+
+  it('should calculate basin size', () => {
+    state = calculateBasins(mapBasins(state));
+    expect(state).toBe(821560);
   });
 });
