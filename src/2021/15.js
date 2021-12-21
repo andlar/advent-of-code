bb5d57901e92688da8f254ba94e16dc8b3137feb
Andrew Larned <andrew.larned@gmail.com>
2021-12-21 05:49:32 -0500

    refactor: separate value from cost

--- src/2021/15.js
+++ src/2021/15.js
@@ -1,27 +1,15 @@
 const getMap = (state) => {
-  return state.values.map((pos) => {
-    let char = `${pos.x === 0 && pos.y !== 0 ? '\n' : ''}${pos.value}`;
-    if (pos.inPath) {
-      char = '\x1b[36m' + char + '\x1b[0m'; // cyan
-    } else if (pos.cost !== undefined) {
-      char = '\x1b[32m' + char + '\x1b[0m'; // green
-    };
-    if (state.fringe.find((frg) => frg.x === pos.x && frg.y === pos.y)) {
-      char = '\x1b[31m' + char + '\x1b[0m'; // red
-    };
-    return char;
-  }).join('');
-};
-
-const getMappedMap = (state) => {
   let out = '';
   for (let y = 0; y < state.size; y++) {
     for (let x = 0; x < state.size; x++) {
-      const loc = state.values[`${x},${y}`];
-      let char = loc.value;
-      if (loc.cost !== undefined) {
+      const loc = state.map[`${x},${y}`];
+      const value = state.values[`${x},${y}`];
+      let char = value;
+      if (loc?.inPath) {
+        char = '\x1b[36m' + char + '\x1b[0m'; // cyan
+      } else if (loc?.cost !== undefined) {
         char = '\x1b[32m' + char + '\x1b[0m'; // green
-    }
+      }
       if (state.fringe.find((frg) => frg === `${x},${y}`)) {
         char = '\x1b[31m' + char + '\x1b[0m'; // red
       };
@@ -33,113 +21,38 @@ const getMappedMap = (state) => {
 };
 
 const parseInput = (input) => {
-  const values = input
-      .flatMap((row, y, ys) => row.map((value, x, xs) => ({
-        value,
-        x,
-        y,
-        cost: x === y && x === 0 ? 0 : undefined,
-      })));
-  return {
-    values,
-    xs: input[0].length,
-    fringe: [].concat({...values[1]}).concat({...values[input[0].length]}),
-  };
-};
-
-const parseInputAsMap = (input) => {
   const values = {};
   input.forEach((row, y) => row.map((value, x) => {
-    values[`${x},${y}`] = {
-        value,
-      cost: x === y && x === 0 ? 0 : undefined,
-    };
+    values[`${x},${y}`] = value;
   }));
   return {
     values,
+    map: { '0,0': { cost: 0 }},
     size: input[0].length,
     fringe: ['0,1', '1,0'],
   };
 };
 
-const manhattanDistance = (a, b) => Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
-
-const findSource = (state, target) => {
-  const neighbor = state.values
-        .filter((source) => source.cost >= 0)
-        .filter((source) => manhattanDistance(source, target) === 1)
-        .sort((a, b) => a.cost - b.cost)[0];
-  return neighbor;
-};
-
-const findTarget = (state) => {
-  const targets = state.fringe.sort((a, b) => (a.heuristic ?? 1) - (b.heuristic ?? 1));
-  return targets[0];
-};
-
-const addIfRelevant = (fringe, item, target, end) => {
-  const next = [...fringe];
-  if (item
-      && item.cost === undefined
-      && !fringe.find((frg) => frg.x === item.x && frg.y === item.y)) {
-    next.push({
-      ...item,
-      heuristic: target.cost + item.value + manhattanDistance(item, end),
-    });
-  }
-  return next;
-};
-
-const expandFringe = (state, target) => {
-  let next = state.fringe
-      .filter((pos) => !(pos.x === target.x && pos.y === target.y));
-  const targetIdx = target.x + target.y * state.xs;
-  if (target.x > 0) {
-    next = addIfRelevant(next, state.values[targetIdx - 1], target, state.values[state.values.length - 1]);
-  }
-  if (target.x < (state.xs - 1)) {
-    next = addIfRelevant(next, state.values[targetIdx + 1], target, state.values[state.values.length - 1]);
-  }
-  next = addIfRelevant(next, state.values[targetIdx - state.xs], target, state.values[state.values.length - 1]);
-  next = addIfRelevant(next, state.values[targetIdx + state.xs], target, state.values[state.values.length - 1]);
-  return next;
-};
-
-const step = (state) => {
-  let target = findTarget(state);
-  const source = findSource(state, target);
-  target = {
-    ...target,
-    cost: source.cost + target.value,
-    source: {x: source.x, y: source.y},
-  }
-  const next = {
-    ...state,
-    fringe: expandFringe(state, target),
-  };
-  next.values[target.x + target.y * state.xs] = target;
-  return next;
-};
-
 const getLocation = (state, key) => {
   const [x, y] = key.split(',').map((v) => parseInt(v, 10));
   const location = {
-    ...state.values[key],
+    ...state.map[key],
+    value: state.values[key],
     x,
     y,
   };
   const neighbors = [
-    {...state.values[`${x - 1},${y}`], key: `${x - 1},${y}`},
-    {...state.values[`${x},${y - 1}`], key: `${x},${y - 1}`},
-    {...state.values[`${x + 1},${y}`], key: `${x + 1},${y}`},
-    {...state.values[`${x},${y + 1}`], key: `${x},${y + 1}`},
+    {...state.map[`${x - 1},${y}`], key: `${x - 1},${y}`},
+    {...state.map[`${x},${y - 1}`], key: `${x},${y - 1}`},
+    {...state.map[`${x + 1},${y}`], key: `${x + 1},${y}`},
+    {...state.map[`${x},${y + 1}`], key: `${x},${y + 1}`},
   ]
         .filter((loc) => loc)
         .filter((loc) => loc.cost >= 0)
         .sort((a, b) => a.cost - b.cost);
   location.source = neighbors[0].key;
   if (!location.cost) {
-    location.heuristic = location.value + (state.size * 2 - x - y - 2) + state.values[location.source].cost;
+    location.heuristic = state.values[key] + (state.size * 2 - x - y - 2) + state.map[location.source].cost;
   }
   return location;
 };
@@ -152,49 +65,41 @@ const findNext = (state) => {
 };
 
 const isRelevant = (state, key) => {
-  return (state.values[key] && state.values[key].cost === undefined && !state.fringe.find((val) => val === key));
+  return (state.values[key] && !state.map[key] && !state.fringe.find((val) => val === key));
 };
 
-const expandFringeInMap = (state, target) => {
+const expandFringe = (state, target) => {
   const { x, y } = target;
   let next = state.fringe
       .filter((pos) => pos !== `${x},${y}`);
-  let key = `${x - 1},${y}`;
-  if (isRelevant(state, key)) {
-    next.push(key);
-  }
-  key = `${x},${y - 1}`;
-  if (isRelevant(state, key)) {
-    next.push(key);
-  }
-  key = `${x + 1},${y}`;
-  if (isRelevant(state, key)) {
-    next.push(key);
-  }
-  key = `${x},${y + 1}`;
-  if (isRelevant(state, key)) {
-    next.push(key);
-  }
+  [
+    `${x - 1},${y}`,
+    `${x},${y - 1}`,
+    `${x + 1},${y}`,
+    `${x},${y + 1}`,
+  ].forEach((key) => {
+    if (isRelevant(state, key)) {
+      next.push(key);
+    }
+  });
   return next;
 };
 
-const stepMap = (state) => {
+const step = (state) => {
   const target = findNext(state);
   const next = {
     ...state,
-    fringe: expandFringeInMap(state, target),
+    fringe: expandFringe(state, target),
   };
-  next.values[`${target.x},${target.y}`].cost = target.value + state.values[target.source].cost;
-  next.values[`${target.x},${target.y}`].source = target.source;
+  next.map[`${target.x},${target.y}`] = {
+    cost: target.value + state.map[target.source].cost,
+    source: target.source,
+  }
   return next;
 };
 
 export {
   getMap,
-  getMappedMap,
   parseInput,
-  parseInputAsMap,
-  findSource,
   step,
-  stepMap,
 };
--- test/2021/15.test.js
+++ test/2021/15.test.js
@@ -1,45 +1,21 @@
 import {
   getMap,
-  getMappedMap,
   parseInput,
-  parseInputAsMap,
-  findSource,
   step,
-  stepMap,
 } from '2021/15';
 import { mock, real } from '2021/15.data';
 
 describe('while parsing data', () => {
   it('should interpret input', () => {
     let state = parseInput(mock);
-    expect(state.xs).toBe(10);
-    expect(state.fringe.length).toBe(2);
-    expect(state.values.length).toBe(100);
-    expect(state.values[0].value).toBe(1);
-    expect(state.values[0].x).toBe(0);
-    expect(state.values[0].y).toBe(0);
-    expect(state.values[0].cost).toBe(0);
-    expect(state.values[11].value).toBe(3);
-    expect(state.values[11].x).toBe(1);
-    expect(state.values[11].y).toBe(1);
-    expect(state.values[11].cost).toBeUndefined();
-    expect(state.values[99].x).toBe(9);
-    expect(state.values[99].y).toBe(9);
-    expect(state.values[99].cost).toBeUndefined();
-  });
-
-  it('should interpret input', () => {
-    let state = parseInputAsMap(mock);
     expect(state.size).toBe(10);
     expect(state.fringe.length).toBe(2);
-    expect(state.values['0,0'].value).toBe(1);
-    expect(state.values['0,0'].cost).toBe(0);
-    expect(state.values['2,0'].value).toBe(6);
-    expect(state.values['2,0'].cost).toBeUndefined();
-    expect(state.values['1,1'].value).toBe(3);
-    expect(state.values['1,1'].cost).toBeUndefined();
-    expect(state.values['9,9'].value).toBe(1);
-    expect(state.values['9,9'].cost).toBeUndefined();
+    expect(state.values['0,0']).toBe(1);
+    expect(state.map['0,0'].cost).toBe(0);
+    expect(state.values['2,0']).toBe(6);
+    expect(state.map['2,0']).toBeUndefined();
+    expect(state.values['1,1']).toBe(3);
+    expect(state.values['9,9']).toBe(1);
   });
 });
 
@@ -49,51 +25,22 @@ describe('with mock data', () => {
     state = parseInput(mock);
   });
 
-  it('should find the previous step', () => {
-    const target = {x: 1, y: 0};
-    const previous = findSource(state, target);
-    expect(previous.x).toBe(0);
-    expect(previous.y).toBe(0);
-  });
-
   it('should take a step', () => {
     const next = step(state);
-    expect(next.values[1].cost).toBe(1);
+    expect(next.map['0,1'].cost).toBe(1);
+    expect(next.map['0,1'].source).toBe('0,0');
     expect(next.fringe.length).toBe(3);
+    //console.log(getMap(next));
   });
 
   it('should find the cost to get to the end', () => {
     let next = step(state);
-    while (!next.values[next.values.length - 1].cost) {
+    while (!next.map['9,9']) {
       next = step(next);
       //console.log(getMap(next));
     }
-    //console.log(getMap(next));
-    expect(next.values[next.values.length - 1].cost).toBe(40);
-  });
-});
-
-describe('with mock data in map form', () => {
-  let state;
-  beforeEach(() => {
-    state = parseInputAsMap(mock);
-  });
-
-  it('should take a step', () => {
-    const next = stepMap(state);
-    expect(next.values['0,1'].cost).toBe(1);
-    expect(next.values['0,1'].source).toBe('0,0');
-    expect(next.fringe.length).toBe(3);
-  });
-
-  it('should find the cost to get to the end', () => {
-    let next = stepMap(state);
-    while (!next.values['9,9'].cost) {
-      next = stepMap(next);
-      //console.log(getMappedMap(next));
-    }
-    //console.log(getMappedMap(next));
-    expect(next.values['9,9'].cost).toBe(40);
+    console.log(getMap(next));
+    expect(next.map['9,9'].cost).toBe(40);
   });
 });
 
@@ -105,29 +52,13 @@ describe('with real data', () => {
 
   it('should find the cost to get to the end', () => {
     let next = step(state);
-    while (!next.values[next.values.length - 1].cost) {
+    while (!next.map['99,99']) {
+    //for (let i = 0; i < 50; i++) {
       next = step(next);
+      //console.log(getMap(next));
+      //console.log(`${i}\n${getMap(next)}`);
     }
     //console.log(getMap(next));
-    expect(next.values[next.values.length - 1].cost).toBe(748);
-  });
-});
-
-describe('with real data in map form', () => {
-  let state;
-  beforeEach(() => {
-    state = parseInputAsMap(real);
-  });
-
-  it('should find the cost to get to the end', () => {
-    let next = stepMap(state);
-    while (!next.values['99,99'].cost) {
-    //for (let i = 0; i < 50; i++) {
-      next = stepMap(next);
-      //console.log(getMappedMap(next));
-      //console.log(`${i}\n${getMappedMap(next)}`);
-    }
-    //console.log(getMappedMap(next));
-    expect(next.values['99,99'].cost).toBe(748);
+    expect(next.map['99,99'].cost).toBe(748);
   });
 });
