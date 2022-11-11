8853c6eaf7bb3896cb4d9a4d7fb63326464cef2f
Andrew Larned <andrew.larned@gmail.com>
2022-11-11 14:00:50 -0500

    feat: help santa find the file size of his list

--- 2015/08/src.js
+++ 2015/08/src.js
@@ -1,11 +1,32 @@
 const countChars = (line) => {
   const code = line.length;
-  let converted = line.slice(1, -1);
-  converted = converted.replace(/\"/, 'Q');
-  converted = converted.replace(/\\/, '|');
+  const converted = line.slice(1, -1)
+        .replace(/QQ/g, '"')
+        .replace(/SS/g, '|')
+        .replace(/UU../g, 'U');
   return [code, converted.length, converted];
 };
 
+const encode = (line) => {
+  const code = line.length;
+  const converted = line.slice(1, -1)
+        .replace(/QQ/g, '||""')
+        .replace(/SS/g, '||||')
+        .replace(/UU../g, '||x..');
+  return [code, converted.length + 6];
+};
+
+const getSize = (line, fn = countChars) => {
+  const [code, str] = fn(line);
+  return Math.abs(code - str);
+};
+
+const getFileSize = (lines, fn = countChars) => lines
+      .reduce((size, line) => size += getSize(line, fn), 0);
+
 export {
   countChars,
+  encode,
+  getSize,
+  getFileSize,
 };
--- 2015/08/test.js
+++ 2015/08/test.js
@@ -1,5 +1,8 @@
 import {
   countChars,
+  getSize,
+  getFileSize,
+  encode,
 } from './src';
 import { mock, real } from './data';
 
@@ -11,24 +14,93 @@ describe('when counting characters', () => {
   });
 
   it('should handle simple characters', () => {
-    console.log({mock});
     const [code, str] = countChars(mock[1]);
     expect(code).toBe(5);
     expect(str).toBe(3);
   });
 
-  xit('should handle escaped quotes', () => {
+  it('should handle escaped quotes', () => {
     const [code, str, converted] = countChars(mock[2]);
-    console.log(mock[2]);
-    expect(converted).toBe('aaaQaaa');
+    expect(converted).toBe('aaa"aaa');
     expect(code).toBe(10);
     expect(str).toBe(7);
   });
 
-  xit('should handle escaped slashes', () => {
+  it('should handle escaped slashes', () => {
     const [code, str, converted] = countChars(mock[3]);
     expect(converted).toBe('aaa|aaa');
     expect(code).toBe(10);
     expect(str).toBe(7);
+  });
+
+  it('should handle escaped slashes', () => {
+    const [code, str, converted] = countChars(mock[4]);
+    expect(converted).toBe('U');
+    expect(code).toBe(6);
+    expect(str).toBe(1);
+  });
+
+  it('should know the size difference', () => {
+    expect(getSize(mock[0])).toBe(2);
+    expect(getSize(mock[1])).toBe(2);
+    expect(getSize(mock[2])).toBe(3);
+    expect(getSize(mock[3])).toBe(3);
+    expect(getSize(mock[4])).toBe(5);
+  });
+
+  it('should know the file size', () => {
+    expect(getFileSize(mock)).toBe(15);
+  });
+
+  it('should know the full file size', () => {
+    expect(getFileSize(real)).toBe(1342);
+  });
+});
+
+describe('when encoding characters', () => {
+  it('should count basic quotes', () => {
+    const [code, str] = encode(mock[0]);
+    expect(code).toBe(2);
+    expect(str).toBe(6);
+  });
+
+  it('should handle simple characters', () => {
+    const [code, str] = encode(mock[1]);
+    expect(code).toBe(5);
+    expect(str).toBe(9);
+  });
+
+  it('should handle escaped quotes', () => {
+    const [code, str] = encode(mock[2]);
+    expect(code).toBe(10);
+    expect(str).toBe(16);
+  });
+
+  it('should handle escaped slashes', () => {
+    const [code, str] = encode(mock[3]);
+    expect(code).toBe(10);
+    expect(str).toBe(16);
+  });
+
+  it('should handle escaped slashes', () => {
+    const [code, str] = encode(mock[4]);
+    expect(code).toBe(6);
+    expect(str).toBe(11);
+  });
+
+  it('should know the size difference', () => {
+    expect(getSize(mock[0], encode)).toBe(4);
+    expect(getSize(mock[1], encode)).toBe(4);
+    expect(getSize(mock[2], encode)).toBe(6);
+    expect(getSize(mock[3], encode)).toBe(6);
+    expect(getSize(mock[4], encode)).toBe(5);
+  });
+
+  it('should know the file size', () => {
+    expect(getFileSize(mock, encode)).toBe(25);
+  });
+
+  it('should know the full file size', () => {
+    expect(getFileSize(real, encode)).toBe(2074);
   });
 });
