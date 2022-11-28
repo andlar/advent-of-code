5b51075469b9f3332c07e535c72a51350a8e6935
Andrew Larned <andrew.larned@gmail.com>
2022-11-28 09:31:14 -0500

    feat: help santa find the shortest and longest routes he can take

--- 2015/09/src.js
+++ 2015/09/src.js
@@ -15,7 +15,37 @@ const interpretInput = (input) => {
   return [distances, new Set(locations)];
 };
 
+const buildRoutes = (locations) => {
+  let routes = [...locations];
+  for (let i = 1; i < locations.size; i++) {
+    routes = routes.flatMap((r) => [...locations]
+                            .filter((v) => !r.includes(v))
+                            .map((v) => `${r}.${v}`))
+  }
+  return routes;
+};
+
+const calculateCost = (distances, route) => route
+      .split('.')
+      .reduce((sum, city, idx, arr) => {
+        if (idx === 0) { return sum; }
+        const key = [city, arr[idx - 1]]
+              .sort((a, b) => (a < b ? -1 : 1))
+              .join('.');
+        return sum + distances[key];
+      }, 0);
+
+const findRoute = (input, shortest = true) => {
+  const [distances, locations] = interpretInput(input);
+  const routes = buildRoutes(locations);
+  const costs = routes.map((r) => calculateCost(distances, r));
+  return shortest ? Math.min(...costs) : Math.max(...costs);
+};
+
 export {
   interpretRow,
   interpretInput,
+  buildRoutes,
+  calculateCost,
+  findRoute,
 };
--- 2015/09/test.js
+++ 2015/09/test.js
@@ -1,10 +1,13 @@
 import {
   interpretRow,
   interpretInput,
+  buildRoutes,
+  calculateCost,
+  findRoute,
 } from './src';
 import { mock, real } from './data';
 
-describe('parsing', () => {
+describe('when parsing', () => {
   it('should interpret a row', () => {
     const [key, distance, locations] = interpretRow(mock[0]);
     expect(key).toBe('Dublin.London');
@@ -25,5 +28,41 @@ describe('parsing', () => {
   it('should interpret all the rows of real data', () => {
     const [distances, locations] = interpretInput(real);
     expect(locations).toEqual(new Set(['AlphaCentauri', 'Arbre', 'Faerun', 'Norrath', 'Snowdin', 'Straylight', 'Tambi', 'Tristram']));
+  });
+});
+
+describe('when working with routes', () => {
+  it('should build the list of routes from a set', () => {
+    const [, locations] = interpretInput(mock);
+    const routes = buildRoutes(locations);
+    expect(routes.length).toBe(6);
+    expect(routes.includes('Dublin.London.Belfast')).toBe(true);
+    expect(routes.includes('fake.foo.bar')).toBe(false);
+  });
+
+  it('should calculate the cost of a route', () => {
+    const [distances,] = interpretInput(mock);
+    expect(calculateCost(distances, 'London.Dublin.Belfast')).toBe(605);
+    expect(calculateCost(distances, 'London.Belfast.Dublin')).toBe(659);
+    expect(calculateCost(distances, 'Dublin.London.Belfast')).toBe(982);
+    expect(calculateCost(distances, 'Dublin.Belfast.London')).toBe(659);
+    expect(calculateCost(distances, 'Belfast.London.Dublin')).toBe(982);
+    expect(calculateCost(distances, 'Belfast.Dublin.London')).toBe(605);
+  });
+
+  it('should find the shortest route', () => {
+    expect(findRoute(mock)).toBe(605);
+  });
+
+  it('should find the shortest route on real data', () => {
+    expect(findRoute(real)).toBe(117);
+  });
+
+  it('should find the longest route', () => {
+    expect(findRoute(mock, false)).toBe(982);
+  });
+
+  it('should find the longest route on real data', () => {
+    expect(findRoute(real, false)).toBe(909);
   });
 });
